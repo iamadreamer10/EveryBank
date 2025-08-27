6ae17e47f85845ffcd0f4491ee124c1c768af244
onyo9 <onyo9ssafy@gmail.com>
2025-08-28 00:34:34 +0900

    backend project auth accesstoken setting

---
+++ everybank-frontend/src/App.tsx
@@ -0,0 +1,28 @@
+import {BrowserRouter as Router, Routes, Route} from "react-router-dom";
+import Header from "./components/common/Header";
+import Footer from "./components/common/Footer";
+import HomePage from "./pages/HomePage";
+import LoginPage from "./pages/auth/LoginPage";
+import SignupPage from "./pages/auth/SignupPage";
+import ProductsPage from "./pages/products/ProductsPage.tsx";
+import ProductDetailPage from "./pages/products/ProductDetailPage.tsx";
+
+export default function App() {
+    return (
+        <Router>
+            <div className="min-h-screen bg-gray-50 flex flex-col">
+                <Header/>
+                <main className="flex-1">
+                    <Routes>
+                        <Route path="/" element={<HomePage/>}/>
+                        <Route path="/login" element={<LoginPage/>}/>
+                        <Route path="/signup" element={<SignupPage/>}/>
+                        <Route path="/products" element={<ProductsPage/>}/>
+                        <Route path="/products/:productType/:productCode" element={<ProductDetailPage />} />
+                    </Routes>
+                </main>
+                <Footer/>
+            </div>
+        </Router>
+    );
+}
